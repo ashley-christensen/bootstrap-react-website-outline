9f1d18e2526ead722653e96a7496e699f98af495
Ashley Christensen <ashleyccode@yahoo.com>
2022-04-21 14:37:41 -0700

    navbar style

--- src/components/inc/Navbar.js
+++ src/components/inc/Navbar.js
@@ -2,43 +2,43 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 const Navbar = () => {
   return (
-    <nav class='navbar navbar-expand-lg navbar-light bg-light'>
-      <div class='container-fluid'>
-        <Link to='/' class='navbar-brand'>
-          Ashley Christensen
-        </Link>
-        <button
-          class='navbar-toggler'
-          type='button'
-          data-bs-toggle='collapse'
-          data-bs-target='#navbarSupportedContent'
-          aria-controls='navbarSupportedContent'
-          aria-expanded='false'
-          aria-label='Toggle navigation'
-        >
-          <span class='navbar-toggler-icon'></span>
-        </button>
-        <div class='collapse navbar-collapse' id='navbarSupportedContent'>
-          <ul class='navbar-nav ms-auto mb-2 mb-lg-0'>
-            <li class='nav-item'>
-              <Link to='/' class='nav-link active'>
-                Home
-              </Link>
-            </li>
-            <li class='nav-item'>
-              <Link to='/about' class='nav-link active'>
-                About
-              </Link>
-            </li>
-            <li class='nav-item'>
-              <Link to='/contact' class='nav-link active'>
-                Contact
-              </Link>
-            </li>
-          </ul>
+    <div className='navbar-dark bg-info'>
+      <div className='container'>
+        <div className='row'>
+          <div className='col-md-12'>
+            <nav class='navbar navbar-expand-lg'>
+              <div class='container-fluid'>
+                <Link to='/' class='navbar-brand'>
+                  Ashley Christensen
+                </Link>
+                <div
+                  class='collapse navbar-collapse'
+                  id='navbarSupportedContent'
+                >
+                  <ul class='navbar-nav ms-auto mb-2 mb-lg-0'>
+                    <li class='nav-item'>
+                      <Link to='/' class='nav-link active'>
+                        Home
+                      </Link>
+                    </li>
+                    <li class='nav-item'>
+                      <Link to='/about' class='nav-link active'>
+                        About
+                      </Link>
+                    </li>
+                    <li class='nav-item'>
+                      <Link to='/contact' class='nav-link active'>
+                        Contact
+                      </Link>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </nav>
+          </div>
         </div>
       </div>
-    </nav>
+    </div>
   );
 };
 
