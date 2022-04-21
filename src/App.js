1d17fafd4c40045978937c6ed49781cd7b8aa8ae
Ashley Christensen <ashleyccode@yahoo.com>
2022-04-21 14:19:38 -0700

    Router finished

--- src/App.js
+++ src/App.js
@@ -9,22 +9,14 @@ function App() {
     <Router>
       <div>
         <Navbar />
-        <Home />
-        <About />
-        <Contact />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='About' element={<About />} />
+          <Route path='Contact' element={<Contact />} />
+        </Routes>
       </div>
     </Router>
   );
 }
 
 export default App;
-
-// <>
-//   <Router>
-//     <Routes>
-//       <Route path='/' element={<Home />} />
-//       <Route path='About' element={<About />} />
-//       <Route path='Contact' element={<Contact />} />
-//     </Routes>
-//   </Router>
-// </>
